a675595b96e9090d3cbc5661fc7f151b506d1e76
rochaenrique <rochabenattienrique@gmail.com>
2024-10-07 22:23:35 +0200

    sesion 2 ajustada y terminada

--- FP Practica 2/src/Sesion2.java
+++ FP Practica 2/src/Sesion2.java
@@ -1,32 +1,25 @@
 import java.util.Scanner;
 
 public class Sesion2 {
-    static boolean esPrimo(long numero) {
-        if (numero == 1)
-            return false;
-        boolean esPrimo = true;
-        for (long i = 2; i < numero; i++)
-        {
-            //System.out.println("Divisor: " + i + ", Numero " + numero);
-            if (numero % i == 0)
-            {
-                esPrimo = false;
-                break;
-            };
-        };
-
-        return esPrimo;
-    };
 
     public static void main(String[] args) {
          System.out.println("Hola!\n1. Introduce a un numero entere 0 y 1000: ");
          Scanner sc = new Scanner(System.in);
-         long numero = sc.nextLong();
+         int numero = sc.nextInt();
 
-         if (esPrimo(numero))
-             System.out.println(numero + " es primo");
-         else
-             System.out.println(numero + " no es un primo");
+         boolean esPrimo = true;
+         if (numero != 1) { 
+             for (int i = 2; i < numero; i++)
+             {
+                 if (numero % i == 0)
+                 {
+                     esPrimo = false;
+                     break;
+                 };
+             };
+         };
+
+         System.out.println(numero + (esPrimo ? "" : " no") + " es un primo");
 
          System.out.print("2. Cuantos numeros primos te gustaria ver (entre 5 y 20)? "); 
          numero = sc.nextInt();
@@ -39,12 +32,25 @@ public class Sesion2 {
              int contadorDePrimos = 0;
              for (int i = 0; contadorDePrimos < numero; i++)
              {
-                 if (esPrimo(i)) {
-                     System.out.print("\t" + i);
+                 esPrimo = true;
+                 if (numero != 1) { 
+                     for (int j = 2; j < numero; j++)
+                     {
+                         if (numero % j == 0)
+                         {
+                             esPrimo = false;
+                             break;
+                         };
+                     };
+                 };
+
+                 if (esPrimo) {
+                     System.out.printf("%5d", i);
                      if (contadorDePrimos++ != 0 && contadorDePrimos % 4 == 0)
                          System.out.print('\n');
                  }
              };
          };
+         sc.close();
     };
 }
