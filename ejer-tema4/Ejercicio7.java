5fa8f5c2a9dacaaaca356df34ca7f5510b96358b
rochaenrique <rochabenattienrique@gmail.com>
2024-10-08 12:37:19 +0200

    Hecjo ele ejercicio7

--- ejer-tema4/Ejercicio7.java
+++ ejer-tema4/Ejercicio7.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 public class Ejercicio7 {
+    public static int leerNumeroPositivo(Scanner sc, String msg) { 
+        System.out.print(msg);
+        int n = sc.nextInt();
+        while (n < 0) { 
+            System.out.printf("Numero invalido. Intenta otra vez: ");
+            n = sc.nextInt();
+        };
+        return n;
+    };
 
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Introduce un primer numero positivo: ");
-        int n1 = sc.nextInt();
-        System.out.println("Introduce un segundo numero positivo: ");
-        int n2 = sc.nextInt();
-
+        int n1 = leerNumeroPositivo(sc, "Introduce un primer numero positivo: ");
+        int n2 = leerNumeroPositivo(sc, "Introduce un segundo numero positivo: ");
 
         System.out.printf("Los numeros perfectos entre %d y %d son:\n", n1, n2);
         while (n1++ < n2) { 
             if (Ejercicio6.numeroPerfecto(n1))
-                System.out.println(n1);
+                System.out.printf("\t%3d%n", n1);
         };
         sc.close();
 
