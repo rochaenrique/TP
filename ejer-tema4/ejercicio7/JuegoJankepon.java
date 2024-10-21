095ab823abb9c255b924ea4bc549b99726414a84
rochaenrique <rochabenattienrique@gmail.com>
2024-10-21 16:24:15 +0200

    Terminado el ejercicio7 del tema4 (es divertido)

--- ejer-tema4/ejercicio7/JuegoJankepon.java
+++ ejer-tema4/ejercicio7/JuegoJankepon.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-enum Jankepon { 
+enum Jankepon  { 
     TIJERAS, PAPEL, PIEDRA, LAGARTO, SPOCK;
 }
 
@@ -8,7 +8,25 @@ public class JuegoJankepon {
     public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.print("Introduzca su jugada (");
+        Jankepon[] plays = Jankepon.values();
+        for (Jankepon play : plays)  
+            System.out.print(play.name().substring(0, 1) + play.name().substring(1).toLowerCase() + ", ");
+        System.out.print("): ");
+
+        Jankepon player = Jankepon.valueOf(sc.next().toUpperCase());
+        System.out.println("Has jugado " + player.name());
+        Jankepon computer = plays[(int)(Math.random() * plays.length) + 1];
+        System.out.println("El computador a jugado " + computer.name());
+
+        int diff = player.ordinal() - computer.ordinal();
+        if ((diff > 0 && diff % 2 == 0) || (diff < 0 && diff % 2 != 0))
+            System.out.println("Has ganado!");
+        else
+            System.out.println("Ha ganado el computador!");
+
+
+        sc.close();
     };
 }
 
